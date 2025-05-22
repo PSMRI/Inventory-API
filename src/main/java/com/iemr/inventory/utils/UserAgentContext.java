da2664886b15d18461c6cbc7ec4d6e592db40f52
Ravi Shanigarapu <ravi.shanigarapu@wipro.com>
2025-05-22 17:15:19 +0530

    jwttoken and user-agent validation

---
+++ src/main/java/com/iemr/inventory/utils/UserAgentContext.java
@@ -0,0 +1,18 @@
+package com.iemr.inventory.utils;
+
+public class UserAgentContext {
+	private static final ThreadLocal<String> userAgentHolder = new ThreadLocal<>();
+
+    public static void setUserAgent(String userAgent) {
+        userAgentHolder.set(userAgent);
+    }
+
+    public static String getUserAgent() {
+        return userAgentHolder.get();
+    }
+
+    public static void clear() {
+        userAgentHolder.remove();
+    }
+
+}
